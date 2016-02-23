24370ced8831e199fc980b70b82a9d9426503b00
Winson Tam <winsontam@gmail.com>
2016-02-23 23:40:10 +0800

    Revert "updated"
    This reverts commit 3259335d9de41c70075053266c3d834d1f24b768.

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,14 @@
-var path         = require( 'path' );
-var webpack      = require( 'webpack' );
-var autoprefixer = require( 'autoprefixer' );
+var path    = require( 'path' );
+var webpack = require( 'webpack' );
 
 var config = {
     entry: {
-        main: './js/main.js'
+        main: "./js/main.js"
     },
     output: {
         path: __dirname + '/build',
-        filename: 'bundle.js',
-        chunkFilename: '[id].bundle.js'
+        filename: "bundle.js",
+        chunkFilename: "[id].bundle.js"
     },
     module: {
         loaders: [
@@ -20,38 +19,15 @@ var config = {
             },
             {
                 test: /\.css$/,
-                loaders: [ 'style', 'css', 'postcss' ]
+                exclude: /(node_modules|bower_components)/,
+                loaders: [ 'style', 'css' ]
             },
             {
                 test: /\.scss$/,
                 exclude: /(node_modules|bower_components)/,
-                loaders: [ 'style', 'css', 'postcss', 'sass' ]
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                loaders: [
-                    'url?limit=1024',
-                    'image-webpack?{progressive:true,optimizationLevel:7,interlaced:false,pngquant:{quality:"65-90",speed:4}}'
-                ]
+                loaders: [ 'style', 'css', 'sass' ]
             }
         ]
-    },
-    postcss: function () {
-        return [
-            autoprefixer( {
-                browsers: [
-                    'Chrome >= 35',
-                    'Firefox >= 38',
-                    'Edge >= 12',
-                    'Explorer >= 9',
-                    'iOS >= 8',
-                    'Safari >= 8',
-                    'Android 2.3',
-                    'Android >= 4',
-                    'Opera >= 12'
-                ]
-            } )
-        ];
     },
     plugins: [
         new webpack.optimize.CommonsChunkPlugin( 'main', null, false ),
